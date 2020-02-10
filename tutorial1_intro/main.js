9da2a1a6cb185eccd1622514358d23c4e34a7051
Valliappan Lakshmanan <valliappan.lakshmanan9@gmail.com>
2020-02-10 17:32:41 -0500

    Added filter logicfor rows

--- tutorial1_intro/main.js
+++ tutorial1_intro/main.js
@@ -24,16 +24,18 @@ thead
 const rows=tbody
     .selectAll("tr")
     .data(data)
-    .join("tr");
-
-// adding in the values for each cell.
-// I tried my best with the logic here. I tried to access highlight a cell in the "PCT"column if it was greater
-// than .500. But only issue is that I did not know what d was here and had a tough time trying to select
-//values only in the column.  
+    .join("tr")
+    .attr("class", (data) => {
+        let tag;
+        if(+data['Pct']>.500){
+           return tag='above500'
+        }else{
+           return tag=null; 
+        };
+    });
 rows
     .selectAll('td')
     .data(d=>Object.values(d))
     .join("td")
-    .attr("class", d => d[3] > .5 ? 'above500' : null)
     .text(d=>d);
 });
